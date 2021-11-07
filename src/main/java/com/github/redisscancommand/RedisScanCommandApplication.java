3a8c0074f214921437c872c584ae58760a91c69c
jinrunheng <1175088275@qq.com>
2021-11-08 02:12:23 +0800

    init project

---
+++ src/main/java/com/github/redisscancommand/RedisScanCommandApplication.java
@@ -0,0 +1,13 @@
+package com.github.redisscancommand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisScanCommandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedisScanCommandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/redisscancommand/RedisScanCommandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.redisscancommand;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisScanCommandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
